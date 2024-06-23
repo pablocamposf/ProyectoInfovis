2153ff15c69f95d1960bf14eaa521ddc5c31b7ea
DanoUC <dealegria@uc.cl>
2024-06-23 00:05:17 -0400

    visualizacion base comparacion

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const HEIGHT_VIS_1 = 250;
 
 SVG1.attr("width", WIDTH_VIS_1).attr("height", HEIGHT_VIS_1);
 
-
 createBubbleChart();
 
 function createBubbleChart() {
@@ -65,4 +64,115 @@ function createBubbleChart() {
     });
 }
 
+// Constants for the second visualization
+const DATAS2_URL = "https://raw.githubusercontent.com/pablocamposf/ProyectoInfovis/comparacion/champions_stats.csv";
+const SVG2 = d3.select("#vis-2").append("svg");
+const WIDTH_VIS_2 = 900;
+const HEIGHT_VIS_2 = 500;
+SVG2.attr("width", WIDTH_VIS_2).attr("height", HEIGHT_VIS_2);
 
+const margin = {top: 20, right: 30, bottom: 200, left: 40}; // Increase bottom margin for text
+const width = WIDTH_VIS_2 - margin.left - margin.right;
+const height = HEIGHT_VIS_2 - margin.top - margin.bottom;
+
+const xScale = d3.scaleBand().range([0, width]).padding(0.1);
+const yScale = d3.scaleLinear().range([height, 0]);
+
+const g = SVG2.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
+
+// Tooltip div
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
+
+// Scale for seasons color
+const seasonColorScale = d3.scaleOrdinal()
+    .domain(d3.range(1, 13)) // Assuming seasons 1 to 12
+    .range(d3.schemeCategory10);
+
+function createScatterPlot(role) {
+    d3.csv(DATAS2_URL, d3.autoType).then(data => {
+        // Use kill_death_assist_ratio directly
+        data.forEach(d => {
+            d.kda = d.kill_death_assist_ratio;
+        });
+
+        // Filter data based on the selected role
+        const filteredData = role === "all" ? data : data.filter(d => d.rol === role);
+
+        // Update scales
+        xScale.domain(filteredData.map(d => `${d.season} ${d.player} ${d.team}`));
+        yScale.domain([0, d3.max(filteredData, d => d.kda)]);
+
+        // Remove previous elements
+        g.selectAll("*").remove();
+
+        // Add x-axis
+        g.append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(xScale))
+            .selectAll("text")
+            .attr("y", 0)
+            .attr("x", 9)
+            .attr("dy", ".35em")
+            .attr("transform", "rotate(45)")
+            .style("text-anchor", "start")
+            .text(function(d) {
+                const parts = d.split(" ");
+                return `S${parts[0]} ${parts[1]}\n${parts[2]}`;
+            });
+
+        // Add y-axis
+        g.append("g")
+            .call(d3.axisLeft(yScale));
+
+        // Add x-axis label
+        g.append("text")
+            .attr("transform", `translate(${width / 2},${height + margin.bottom - 80})`)
+            .style("text-anchor", "middle")
+            .text("Jugador Campeón");
+
+        // Add y-axis label
+        g.append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 0 - margin.left)
+            .attr("x", 0 - (height / 2))
+            .attr("dy", "1em")
+            .style("text-anchor", "middle")
+            .text("KDA");
+
+        // Add dots
+        g.selectAll(".dot")
+            .data(filteredData)
+            .enter()
+            .append("circle")
+            .attr("class", "dot")
+            .attr("cx", d => xScale(`${d.season} ${d.player} ${d.team}`) + xScale.bandwidth() / 2)
+            .attr("cy", d => yScale(d.kda))
+            .attr("r", 5)
+            .style("fill", d => seasonColorScale(d.season))
+            .on("mouseover", function(event, d) {
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9);
+                tooltip.html(`KDA: ${d.kda}`)
+                    .style("left", (event.pageX + 5) + "px")
+                    .style("top", (event.pageY - 28) + "px");
+            })
+            .on("mouseout", function(d) {
+                tooltip.transition()
+                    .duration(500)
+                    .style("opacity", 0);
+            });
+    });
+}
+
+// Initial rendering of scatter plot with all roles
+createScatterPlot("all");
+
+// Add event listener to the role selector
+d3.select("#role-select").on("change", function() {
+    const selectedRole = d3.select(this).property("value");
+    createScatterPlot(selectedRole);
+});
